e592dd4b081e0735848e0c89eef6866b03d1d038
Nikolay Klokov <kklokov@inbox.ru>
2022-05-29 11:38:38 +0400

    Create repository of Mod

---
+++ Source/AdvStarWars/AdvStarWars/StarExecuter.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+
+namespace AdvStarWars
+{
+    [StaticConstructorOnStartup]
+    public static class StarExecuter
+    {
+        public const string Name = "AdvancedStarWars: ";
+
+        static StarExecuter()
+        {
+            Verse.Pawn
+            Log.Message(Name + "Constructing ...");
+        }
+        
+    }
+}
